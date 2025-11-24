d21e9e779625de18b477e74d16c01501bb8b929d
Gustavo <gustavo.javax@gmail.com>
2025-11-24 10:22:35 -0300

    add arquivos faltantes

---
+++ src/main/java/br/com/catedral/visitacao/VisitacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.catedral.visitacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/catedral/visitacao/VisitacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catedral.visitacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
